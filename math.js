afb5ffa4fdfb4ce353a7c8cc0560a1dea10db5e8
ralphvanraaij <rvanraaij@gmail.com>
2025-02-01 13:01:25 +0100

    Added math constants for truncated dodecahedron

--- math.js
+++ math.js
@@ -1,10 +1,14 @@
 // mathematical constants
 export const PI = Math.PI;
 export const SQRT2 = Math.sqrt(2);
-export const PHI   = (1 + Math.sqrt(5)) / 2;
-export const TRIB  = 1.83928675521416;  // tribonacci constant
-export const SNUB  = Math.sqrt(2 + (4 * TRIB) - 2 * (TRIB * TRIB));  // used for snub cube (l) and snub cube (r)
+export const PHI = (1 + Math.sqrt(5)) / 2;
+export const TRIB = 1.83928675521416;  // tribonacci constant
+export const SNUB = Math.sqrt(2 + (4 * TRIB) - 2 * (TRIB * TRIB));  // used for snub cube (l) and snub cube (r)
 export const TRUNCUBE = 2 * (SQRT2 - 1); // used for truncated cube
+export const TRDO1 = (1 + Math.sqrt(5)) / 2; // used for truncated dodecahedron; NB equals PHI
+export const TRDO2 = (2 + Math.sqrt(5)) / 2; // used for truncated dodecahedron
+export const TRDO3 = (3 + Math.sqrt(5)) / 2; // used for truncated dodecahedron
+export const TRDO4 = (5 + (3 * Math.sqrt(5))) / 4; // used for truncated dodecahedron
 
 // mathematical functions
 export const Sin = Math.sin;
