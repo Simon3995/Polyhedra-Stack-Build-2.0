dc5d22622026adc791f3755957df42f3a2efb4d3
shnimon <33228504+Simon3995@users.noreply.github.com>
2025-02-06 21:26:05 +0100

    Implemented theme switching

--- themes.js
+++ themes.js
@@ -1,5 +1,6 @@
 import translucent from './themes/translucent.js';
 import basic from './themes/basic.js';
+import { Scene } from './main.js';
 
 const Themes = {};
 
@@ -7,3 +8,20 @@ Themes["Translucent"] = translucent;
 Themes["Basic"] = basic;
 
 export default Themes;
+
+
+document.getElementById("themeselect").onchange = function(e) {
+    Scene.theme = Themes[e.target.value];
+    reload_theme(Scene.scene);
+}
+
+export const reload_theme = function (scene) {
+    for (const obj of scene.children) {
+        if (obj.type == "LineSegments") {
+            obj.material = Scene.theme.line_material;
+            reload_theme(obj);
+        } else if (obj.type == "Mesh") {
+            obj.material = Scene.theme.default;
+        }
+    }
+}
