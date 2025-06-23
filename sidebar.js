c659c7dba75bde04efb7dc9bcade59e1064e9acb
Simon van Raaij <33228504+Simon3995@users.noreply.github.com>
2025-06-23 12:40:05 +0200

    Cleaned up generate_polyhedra_list function

--- sidebar.js
+++ sidebar.js
@@ -2,55 +2,48 @@ import Shapes from './shapes.js';
 import { Scene } from './main.js';
 import { create_shape } from './model.js';
 
-/*export const generate_polyhedra_list = function() {
+export const generate_polyhedra_list = function() {
+    // clear existing polyhedra list
     const list = document.getElementById("polyhedralist");
     list.innerHTML = "";
 
-    for (let i = 0; i < 20; i++) {
-        let div = `<button class="polyhedrabutton">`;
-        div += `Rhombicosi&shy;dodecahedron`;
-        div += `</button>`;
-
-        list.innerHTML += div;
-    }
-}*/
-
-export const generate_polyhedra_list = function() {
-    let list = document.getElementById("polyhedralist");
-    list.innerHTML = "";
-
+    // create a list of polyhedra grouped by type
     const polyhedra_array = Object.entries(Shapes).map(([key, value]) => ({
         ...value,
         name: key
     }));
     const grouped_shapes = Object.groupBy(polyhedra_array, ({category}) => category);
 
-    console.log(grouped_shapes);
-
+    // add all shapes per-category
     for (const category in grouped_shapes) {
-        /*list.innerHTML += `<h2>${category}</h2>`;*/
         for (const shape of grouped_shapes[category]) {
-            const id = (category + "_" + shape.name).replace(/\W/g, '');
-            list.innerHTML += `<button id="${id}" class="polyhedrabutton${(shape.name == Scene.add_shape) ? ' selected' : ''}">${shape.name}</button>`;
-            
-            // dit is een rommeltje en ik ga het nog opruimen maar niet nu lol
-            setTimeout(function() {
-                document.getElementById(id).onclick = function() {
-                    Scene.add_shape = shape.name;
-                    
-                    // deselect all buttons
-                    let selected = document.getElementsByClassName("selected");
-                    selected[0].className = "polyhedrabutton";
+            // create new button element
+            let button = document.createElement("button");
 
-                    // select this button
-                    this.className = "polyhedrabutton selected";
+            // add classes
+            button.classList.add("polyhedrabutton");
+            if (shape.name == Scene.add_shape) button.classList.add("selected");
 
-                    // if scene is empty, create new
-                    if (Scene.scene.children.filter(x => x.type == "LineSegments").length == 0) {
-                        Scene.scene.add(create_shape(Scene.add_shape));
-                    }
+            // attach onclick event listener
+            button.onclick = function() {
+                Scene.add_shape = shape.name;
+                
+                // deselect all buttons
+                let buttons = document.getElementsByClassName("polyhedrabutton");
+                for (let b of buttons) b.classList.remove("selected");
+
+                // select this button
+                this.classList.add("selected");
+
+                // if scene is empty, create new
+                if (Scene.scene.children.filter(x => x.type == "LineSegments").length == 0) {
+                    Scene.scene.add(create_shape(Scene.add_shape));
                 }
-            }, 20);
+            }
+
+            // add button text and add to polyhedra list
+            button.innerHTML = shape.name;
+            list.appendChild(button);
         }
     }
 }
